7e0f1c2dbd71682b26a933d732652ab6d5ed8964
JAmal Michael <eshimi090@gmail.com>
2024-06-16 03:23:46 -1200

    mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
 
 const app = express();
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routes
+// app.use('server/users', userRoutes);
+
+//connection
+mongoose.connect(process.env.CONNECTION_STRING, {
+}).then(
+    console.log("MongoDB Connected")
+).catch(err => console.log(err));
 
 const port = 3000;
 
